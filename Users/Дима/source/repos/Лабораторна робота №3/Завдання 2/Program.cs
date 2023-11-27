ae8d7c8d971f3c3a2b64e3c35cb0faabb879b282
{{Luzhin55}} <{{superdimon25@gmail.com}}>
2023-11-27 06:18:30 +0300

    first commit

---
+++ Users/Дима/source/repos/Лабораторна робота №3/Завдання 2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point A = new Point(1, 1, "A");
+        Point B = new Point(2, 4, "B");
+        Point C = new Point(5, 3, "C");
+
+        Figure triangle = new Figure(A, B, C);
+        triangle.CalculatePerimeter();
+    }
+}
---
+++ Users/Дима/source/repos/Лабораторна робота №3/Лабораторна робота №3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть довжину першої сторони прямокутника:");
+        double side1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введіть довжину другої сторони прямокутника:");
+        double side2 = Convert.ToDouble(Console.ReadLine());
+
+        Rectangle rectangle = new Rectangle(side1, side2);
+
+        Console.WriteLine($"Периметр прямокутника: {rectangle.Perimeter}");
+        Console.WriteLine($"Площа прямокутника: {rectangle.Area}");
+    }
+}
+
